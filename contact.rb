0c529bfbe762d25f03612f587eeaff754c4139cb
Paraprax <parkerjohnston28@gmail.com>
2017-04-09 22:06:13 -0400

    swappedd the original contact class file for the stock sqlite3 one, made necessary syntax changes

--- contact.rb
+++ contact.rb
@@ -1,137 +1,26 @@
-class Contact
-
-  # Class variables and methods
-
-  @@contacts = []
-  @@id = 1
-
-  def self.create(first_name, last_name, email, note)
-    contact=Contact.new(first_name, last_name, email, note)
-    @@contacts << contact
-    return contact
-  end
+gem 'activerecord', '=4.2.7'
+require 'active_record'
+require 'mini_record'
 
 
-  def self.all
-    @@contacts
-  end
+#connect to sql database
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm-web.sqlite3')
 
-    def self.find_by_anything
-        input = gets.chomp
-        puts "~ ~ ~ ~ ~ ~ ~ ~"
-        puts "Contact found:"
-    @@contacts.each do |contact|
-      if contact.first_name == input
-        puts contact.print_contact
-      elsif contact.last_name == input
-         puts contact.print_contact
-      elsif contact.email == input
-         puts contact.print_contact
-      elsif contact.note == input
-         puts contact.print_contact
-       elsif contact.id == input.to_i
-          puts contact.print_contact
-      else
-        end
-    end
-  end
+class Contact < ActiveRecord::Base
 
-  def self.delete_contact
-    input = gets.chomp
-    puts "~ ~ ~ ~ ~ ~ ~ ~"
-    puts "Contact found:"
-@@contacts.each do |contact|
-    if contact.id == input.to_i
-      puts contact.print_contact
-      puts contact.delete
-    elsif contact.email == input
-        puts contact.print_contact
-        puts contact.delete
-    else
-      end
-    end
-  end
+  # attr_accessor :first_name, :last_name, :email, :note
+  # attr_reader :id
 
-  def delete
-    @@contacts.delete(self)
-  end
-
-
-  def self.find_by(attribute, value)
-    case attribute
-    when "first_name"
-      @@contacts.select {|contact| contact.first_name == value}
-    when "last_name"
-      @@contacts.select {|contact| contact.last_name == value}
-    when "email"
-      return self if self.email == value
-    when "note"
-      return self if self.note == value
-    end
-  end
-
-  def self.find(contact_id)
-    @@contacts.each do |contact|
-      if contact.id == contact_id
-        return contact
-      end
-    end
-  end
-
-
-  def self.delete_all
-    @@contacts.clear
-  end
-
-
-  def update(attribute, value)
-    case attribute
-    when "first_name"
-      self.first_name = value
-    when "last_name"
-      self.last_name = value
-    when "email"
-      self.email = value
-    when "note"
-      self.note = value
-    end
-    return self
-  end
-
-  attr_accessor :first_name
-  attr_accessor :last_name
-  attr_accessor :email
-  attr_accessor :note
-  attr_reader   :id
-
-  def initialize(first_name, last_name, email, note)
-    @first_name = first_name
-    @last_name = last_name
-    @email = email
-    @note = note
-    @id = @@id
-    @@id += 1
-  end
+  field :first_name, as: :string
+  field :last_name, as: :string
+  field :email, as: :string
+  field :note, as: :text
 
 
   def full_name
-    return "#{first_name} #{last_name}"
-  end
-
-  def print_contact
-    print "##{self.id}, #{self.first_name} #{self.last_name}, #{self.email}, note: #{self.note}"
-  end
-
-  def self.print_all_contacts
-    @@contacts.each do |contact|
-      puts "##{contact.id}, #{contact.first_name} #{contact.last_name}, #{contact.email}, note: #{contact.note}"
-    end
+    first_name + " " + last_name
   end
 
 end
 
-# Then, run the following at the command line:
-#
-#   ruby test/contact_test.rb
-#
-# If your implementation of the Contact class is 'right', then you should see that all tests pass!
+Contact.auto_upgrade!
--- crm.rb
+++ crm.rb
@@ -1,13 +1,12 @@
 require_relative 'contact'
 require 'sinatra'
 
-Contact.create('Lois', 'Lane', 'll@dailyplanet.com', 'Reporter')
-Contact.create('Clark', 'Kent', 'ck@dailyplanet.com', 'Reporter')
-Contact.create('Jimmy', 'Olsen', 'supermanspal@dailyplanet.com', 'Photographer')
-Contact.create('Perry', 'White', 'chief@dailyplanet.com', 'Editor-In-Chief')
-Contact.create('Lex', 'Luthor', 'lex@lexcorp.com', 'C.E.O.')
-Contact.create('Gus', 'Gorman', 'gus@webscoe.com', 'Programmer')
-
+#Contact.create('Lois', 'Lane', 'll@dailyplanet.com', 'Reporter')
+#Contact.create('Clark', 'Kent', 'ck@dailyplanet.com', 'Reporter')
+#Contact.create('Jimmy', 'Olsen', 'supermanspal@dailyplanet.com', 'Photographer')
+#Contact.create('Perry', 'White', 'chief@dailyplanet.com', 'Editor-In-Chief')
+#Contact.create('Lex', 'Luthor', 'lex@lexcorp.com', 'C.E.O.')
+#Contact.create('Gus', 'Gorman', 'gus@webscoe.com', 'Programmer')
 
 get '/' do
   @crm_app_name = "METROLODEX"
@@ -22,16 +21,47 @@ get '/contacts/new' do
  erb :new_contact
 end
 
+post '/contacts' do
+  contact = Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note],
+  )
+  redirect to('/')
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :display_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.update(first_name: params[:first_name])
+    @contact.update(last_name: params[:last_name])
+    @contact.update(email: params[:email])
+    @contact.update(note: params[:note])
+    redirect to('/')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 get '/about' do
   erb :about
-end
-
-get '/contacts/edit' do
-  erb :edit_contact
-end
-
-
-post '/contacts' do
-   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-   redirect to('/contacts')
 end
